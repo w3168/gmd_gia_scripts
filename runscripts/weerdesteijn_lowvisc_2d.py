90fbaab815a027bc6443ff18be6f9340090e23d7
w3168 <wis15@ic.ac.uk>
2024-10-03 19:09:21 +1000

    Some extra scripts and small update

---
+++ runscripts/weerdesteijn_lowvisc_2d.py
@@ -0,0 +1,41 @@
+# 2d adjoint model based on Weerdesteijn et al 2023
+
+from gadopt import *
+from weerdesteijn_2d import Weerdesteijn2d
+
+
+class LowViscosityWeerdesteijn2d(Weerdesteijn2d):
+    name = "weerdesteijn-low-viscosity-2d-squashed"
+    vertical_component = 1
+
+    def __init__(self, **kwargs):
+        super().__init__(low_viscosity_region=True, **kwargs)
+
+    def setup_ramp(self):
+        # Seems like you need domain for constant but I thought this had
+        # depreciated?
+        self.ramp = Constant(1)
+
+    def setup_ice_load(self):
+        super().setup_ice_load()
+        # Only update the ice load at initial times
+        self.ice_load.interpolate(self.ramp * self.rho_ice * self.g * self.Hice * self.disc)
+
+    def update_ramp(self):
+        # already initialised with 1 for instantaneous loading
+        pass
+
+    def update_ice_load(self):
+        # interpolating ice load at each timestep breaks adjoint (and is probably not 'adjointable')
+        pass
+
+    def checkpoint_filename(self):
+        return f"{self.name}-dx{round(self.dx/1000)}km-nz{self.nz}-dt{self.dt_years}years-low-viscosity{self.low_viscosity_region}-chk.h5"
+
+    def displacement_filename(self):
+        return f"displacement-{self.name}-dx{round(self.dx/1000)}km-nz{self.nz}-dt{self.dt_years}years-low-viscosity{self.low_viscosity_region}.dat"
+
+
+if __name__ == "__main__":
+    simulation = LowViscosityWeerdesteijn2d(dx=5e3, nz=160, Tend_years=5000, do_write=True)
+    simulation.run_simulation()
