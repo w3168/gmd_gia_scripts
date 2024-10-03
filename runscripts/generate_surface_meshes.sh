46b0466df66827368e172a648765a1868c59ab36
w3168 <wis15@ic.ac.uk>
2024-10-03 13:23:28 +1000

    Fix overwite accident

--- runscripts/generate_surface_meshes.sh
+++ runscripts/generate_surface_meshes.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Generates three box meshes with refined regions near the ice load
-# 'refined_dx' is the the horizontal and vertical resolution in km.
-# The '-' is needed to prevent gmsh from creating another 3d mesh 
+# Generates three surface meshes with refined regions near the ice load
+# 'refined_dx' is the the horizontal resolution in km.
+# The '-' is needed to prevent gmsh from creating another 2d mesh 
 # file with the default name. Maybe there is a neater way of doing this!
-gmsh - weerdesteijn_box_refined.geo -setnumber refined_dx 5
-gmsh - weerdesteijn_box_refined.geo -setnumber refined_dx 10
-gmsh - weerdesteijn_box_refined.geo -setnumber refined_dx 20
-gmsh - weerdesteijn_box_refined.geo -setnumber refined_dx 6.25
-gmsh - weerdesteijn_box_refined.geo -setnumber refined_dx 2.5
+gmsh - weerdesteijn_box_refined_surface.geo -setnumber refined_dx 5
+gmsh - weerdesteijn_box_refined_surface.geo -setnumber refined_dx 10
+gmsh - weerdesteijn_box_refined_surface.geo -setnumber refined_dx 20
