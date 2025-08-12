ede0da4f732289556c54e96796b386d9cb7ca2a3
w3168 <wis15@ic.ac.uk>
2025-08-12 16:57:04 +1000

    Separate both ice/visc figures, add ice rings

--- both_inversion/icemesh/plot_figure12_both_icevisc_inversion.py
+++ both_inversion/icemesh/plot_figure12_both_icevisc_inversion.py
@@ -37,42 +37,41 @@ ice_halo_1d = np.array(ice_df_1d[f'surface_ice_step100'])
 ice_unsorted_1d = ice_halo_1d[condition_1d]
 ice_1d = ice_unsorted_1d[index_1d]
 
-fig, ax = plt.subplots(2, 2, figsize=(6.5,6.5), dpi=300, layout="constrained")
+fig, ax = plt.subplots(1, 2, figsize=(8,3.5), dpi=300, layout="constrained")
 
 # fonts and linewidths etc
 fs = 9
 fs_lab = 10
 ms = 0.75
 lw = 0.75
-ax[0,0].plot(theta_1d, ice_1d_10, color='g', linestyle='-', linewidth=lw+0.5,alpha=0.3, label='Step 10')
-ax[0,0].plot(theta_1d, ice_1d, color='b', linestyle='-', linewidth=lw+0.5,alpha=0.4, label='Step 100')
-ax[0,0].plot(theta_1d, target_ice, color='k', linestyle='--', linewidth=lw, label='Target')
+ax[0].plot(theta_1d, ice_1d_10, color='g', linestyle='-', linewidth=lw+0.5,alpha=0.3, label='Iteration 10')
+ax[0].plot(theta_1d, ice_1d, color='b', linestyle='-', linewidth=lw+0.5,alpha=0.4, label='Iteration 100')
+ax[0].plot(theta_1d, target_ice, color='k', linestyle='--', linewidth=lw, label='Target')
 
 plt.xticks(fontsize=fs)
 plt.yticks(fontsize=fs)
-# Plot short, 1D burgers
-ax[0,0].set_xlabel(r'$\theta$ ($^\circ$)', fontsize=fs_lab)
-ax[0,0].set_ylabel('Normalised ice thickness', fontsize=fs_lab)
-ax[0,0].grid(True, linestyle='dotted')
-ax[0,0].tick_params(axis='both', which='major', labelsize=fs)
-ax[0,0].annotate(
+ax[0].set_xlabel(r'$\theta$ ($^\circ$)', fontsize=fs_lab)
+ax[0].set_ylabel('Normalised ice thickness', fontsize=fs_lab)
+ax[0].grid(True, linestyle='dotted')
+ax[0].tick_params(axis='both', which='major', labelsize=fs)
+ax[0].annotate(
         "a",
         xy=(0.01, 1), xycoords='axes fraction',
         xytext=(1, -1), textcoords='offset fontsize',
         fontsize=fs_lab, verticalalignment='top',
         bbox=dict(facecolor='white', edgecolor='black', lw=lw, pad=5))
-ax[0,0].legend(loc='upper right', fontsize=fs-0.75, framealpha=1, facecolor='white', edgecolor='black', fancybox=False).get_frame().set_linewidth(lw)
+ax[0].legend(loc='upper right', fontsize=fs-0.75, framealpha=1, facecolor='white', edgecolor='black', fancybox=False).get_frame().set_linewidth(lw)
 
 # plot objective function through time
 
 obj = np.loadtxt('adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_functional.txt')
 
-ax[0,1].semilogy(obj[:101], color='k', linestyle='-', linewidth=lw, )
-ax[0,1].set_xlabel('Iteration number', fontsize=fs_lab)
-ax[0,1].set_ylabel('Normalised objective function', fontsize=fs_lab)
-ax[0,1].grid(True, linestyle='dotted')
-ax[0,1].tick_params(axis='both', which='major', labelsize=fs)
-ax[0,1].annotate(
+ax[1].semilogy(obj[:101], color='k', linestyle='-', linewidth=lw, )
+ax[1].set_xlabel('Iteration number', fontsize=fs_lab)
+ax[1].set_ylabel('Objective function', fontsize=fs_lab)
+ax[1].grid(True, linestyle='dotted')
+ax[1].tick_params(axis='both', which='major', labelsize=fs)
+ax[1].annotate(
         "b",
         xy=(0.87, 1), xycoords='axes fraction',
         xytext=(1, -1), textcoords='offset fontsize',
@@ -80,37 +79,8 @@ ax[0,1].annotate(
         bbox=dict(facecolor='white', edgecolor='black', lw=lw, pad=5))
 #ax[1].legend(loc='upper right', fontsize=fs-0.75, framealpha=1, facecolor='white', edgecolor='black', fancybox=False).get_frame().set_linewidth(lw)
 
-# plot viscosity at iteration 100
-visc_10 = plt.imread(f'visc_plots/both_visc_noreg_rerunlithvisc_step10.png')
-ax[1,0].imshow(visc_10)
-ax[1,0].axis('off')
-
-# plot viscosity at iteration 100
-visc = plt.imread(f'visc_plots/both_visc_noreg_rerunlithvisc_step100.png')
-ax[1,1].imshow(visc)
-ax[1,1].axis('off')
-
-# Add colour bars
-# visc cbar
-visc_cmap = plt.get_cmap("inferno_r", 25)
-visc_cbar = ScalarMappable(norm=colors.LogNorm(vmin=1e20, vmax=1e25), cmap=visc_cmap)
-fig.colorbar(visc_cbar,
-             ax=ax[1,0], orientation='horizontal', label='Viscosity (Pa s)',shrink=0.7) # ticks=[1e20, 1e21, 1e22, 1e23,1e24,1e25])
-ax[1,0].annotate(
-        "c",
-        xy=(0.01, 1), xycoords='axes fraction',
-        xytext=(1, -1), textcoords='offset fontsize',
-        fontsize=fs_lab, verticalalignment='top',
-        bbox=dict(facecolor='white', edgecolor='black', lw=lw, pad=5))
-
-ax[1,1].annotate(
-        "d",
-        xy=(0.87, 1), xycoords='axes fraction',
-        xytext=(1, -1), textcoords='offset fontsize',
-        fontsize=fs_lab, verticalalignment='top',
-        bbox=dict(facecolor='white', edgecolor='black', lw=lw, pad=5))
 
 
-figname = "Figure_12_both_ice_visc_obj_inversion_step10v100_29.07.25"
+figname = "Figure_12_both_ice_visc_obj_inversion_step10v100_12.08.25_justice"
 fig.savefig(f'{figname}.png')
 
--- both_inversion/icemesh/plot_visc_solution.py
+++ both_inversion/icemesh/plot_visc_solution.py
@@ -11,28 +11,44 @@ height = round(height_inches*dpi)
 
 # camera setting
 radius = 2.2
-zoom = 3.8
+zoom =4.25
 
 #steps = [0, 1, 5, 10, 20, 30, 40, 50, 100, 200]
-steps =[ 10, 20, 30 , 40, 50, 100, 200]
+steps =[ 10, 100]
 
-# read disc file to add outline at top and bottom surface of domain
-reader_disc = pv.get_reader('discfile_0.vtu')
-data_disc = reader_disc.read()
-surf = data_disc.extract_feature_edges(boundary_edges=True, non_manifold_edges=False,
-                                  feature_edges=False, manifold_edges=False)
-sphere = pv.Sphere(radius=0.8*radius)
-clipped_surf = surf.clip_surface(sphere, invert=False)
 lw = 5
 
 for s in steps:
     # Read the PVD file
 #    visc_file = f"/data/viscoelastic/internal_variable_adjoint/adjoint/setonix/visc_only/adjoint-cylinder-2d-internalvariable-ctypeviscosity-visconly_icesmooth0.0_icedamp0.0_viscsmooth1e-6_viscdamp1e-5_checkmisfit3_sol/adjoint-cylinder-2d-internalvariable-ctypeviscosity-visconly_icesmooth0.0_icedamp0.0_viscsmooth1e-6_viscdamp1e-5_checkmisfit3_sol_{s}.pvtu"
     #visc_file = f"/data/viscoelastic/internal_variable_adjoint/adjoint/setonix/both/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_sol/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_sol_{s}.pvtu"
-    visc_file = f"/data/viscoelastic/internal_variable_adjoint/adjoint/setonix/both/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth1e-6_viscdamp1e-5_maxrad1e20_sol/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth1e-6_viscdamp1e-5_maxrad1e20_sol_{s}.pvtu"
-
+    visc_file = f"/data/viscoelastic/internal_variable_adjoint/adjoint/setonix/both/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_maxrad1e20_sol/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_maxrad1e20_sol_{s}.pvtu"
+    
     reader = pv.get_reader(visc_file)
     data = reader.read()  # MultiBlock mesh with only 1 block
+    
+    ice_file = f"/data/viscoelastic/internal_variable_adjoint/adjoint/setonix/both/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_maxrad1e20_sol/one_core/adjoint-cylinder-2d-internalvariable-ctypeboth-lithvisc_icemesh_icesmooth0.0_icedamp0.0_viscsmooth0.0_viscdamp0.0_maxrad1e20_sol_{s}_1core.vtu"
+    reader_ice = pv.get_reader(ice_file)
+    data_ice = reader_ice.read()
+
+    # Make ice ring: Extract boundary surface, remove inner surface and expand ring width
+    surf = data_ice.extract_feature_edges(boundary_edges=True, non_manifold_edges=False,
+                                      feature_edges=False, manifold_edges=False)
+    sphere = pv.Sphere(radius=0.8*radius)
+    clipped_surf = surf.clip_surface(sphere, invert=False)
+
+    # Stretch line by 20%
+    stretch = 1.1
+    transform_matrix = np.array(
+        [
+            [stretch, 0, 0, 0],
+            [0, stretch, 0, 0],
+            [0, 0, stretch, 0],
+            [0, 0, 0, 1],
+        ])
+    transformed_surf = clipped_surf.transform(transform_matrix)
+    ice_cmap = plt.get_cmap("Blues", 25)
+    ice_lw = 28
 
 
     data['updated viscosity'] *= 1e21
@@ -57,13 +73,22 @@ for s in steps:
         log_scale=True,
         show_scalar_bar=False,
     )
+    
+    # make ice thickness dimensional
+    transformed_surf['updated ice thickness'] *= 1000
+    
+
+    # add ice ring to image
+    plotter.add_mesh(transformed_surf,color='black', line_width=ice_lw+2, lighting=False, show_scalar_bar=False)
+    plotter.add_mesh(transformed_surf, scalars='updated ice thickness', line_width=ice_lw, clim=[0, 2000], cmap=ice_cmap, lighting=False, show_scalar_bar=False)
+    
     plotter.camera_position = [(0, 0, radius*zoom),
                                      (0.0, 0.0, 0.0),
                                      (0.0, 1.0, 0.0)]
 
-    plotter.screenshot(f"visc_plots/both_visc_smooth1e-6_damp1e-5_lithvisc_step{s}.png")
+    plotter.screenshot(f"visc_plots/both_visc_noreg_lithvisc_icering_step{s}.png")
    
-
+    '''
     # Plot misfit
     data['viscosity'] *= 1e21
     
@@ -134,3 +159,4 @@ for s in steps:
                                      (0.0, 1.0, 0.0)]
 
     plotter.screenshot(f"visc_adj/visc3d_nodampingsmoothlithvisc_adj_step{s}.png")
+    '''
