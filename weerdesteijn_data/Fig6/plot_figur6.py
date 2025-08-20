3b1fff7648bdba387bd7d83064eb06fbf92d77ad
w3168 <wis15@ic.ac.uk>
2025-08-21 09:44:50 +1000

    checking coupled power law
    seems like it converges 'faster' i.e. with larger dt

--- weerdesteijn_data/Fig6/plot_figur6.py
+++ weerdesteijn_data/Fig6/plot_figur6.py
@@ -42,7 +42,14 @@ power_pt2_dt125_nz20 = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-int
 power_pt2_dt250 = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_snes_dt250_dev_thresh1e-16_lag_updm-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt250.0years-bulk1.94-compbuoyTrue-powerlawTrue-nondim.dat")
 power_pt2_dt500 = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_snes_dt500_dev_thresh1e-16_lag_updm-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt500.0years-bulk1.94-compbuoyTrue-powerlawTrue-nondim.dat")
 power_pt2_dt1000 = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_snes_dt1000_dev_thresh1e-16_lag_updm-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt1000.0years-bulk1.94-compbuoyTrue-powerlawTrue-nondim.dat")
+power_pt2_dt2000 = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_snes_dt2000_dev_thresh1e-16_lag_updm-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt2000.0years-bulk1.94-compbuoyTrue-powerlawTrue-nondim.dat")
 
+# power law coupled
+power_pt2_dt125_coupled = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_dt125_coupled_scale1e6_fix-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt125.0years-bulk1.94-compbuoyTrue-nondim.dat")
+power_pt2_dt250_coupled = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_dt250_coupled_scale1e6_fix-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt250.0years-bulk1.94-compbuoyTrue-nondim.dat")
+power_pt2_dt500_coupled = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_dt500_coupled_scale1e6_fix-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt500.0years-bulk1.94-compbuoyTrue-nondim.dat")
+power_pt2_dt1000_coupled = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_dt1000_coupled_scale1e6_fix-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt1000.0years-bulk1.94-compbuoyTrue-nondim.dat")
+power_pt2_dt2000_coupled = np.loadtxt(f"{long_power}displacement-weerdesteijn-3d-internalvariable-power_n3_transstress0.2_dt2000_coupled_scale1e6_fix-refinedsurfaceTrue-dx5.0km-nz10perlayer-dt2000.0years-bulk1.94-compbuoyTrue-nondim.dat")
 
 times_dt10 = np.arange(0, 210, 10)
 times_dt5 = np.arange(5, 205, 5)
@@ -62,7 +69,7 @@ ax[0].plot(times_dt10, vr0pt1_nz10_dx5_dt10[:21], color=colour_burgers_10, linew
 plt.xticks(fontsize=fs)
 plt.yticks(fontsize=fs)
 # Plot short, 1D burgers
-ax[0].set_xlabel('Time (a)', fontsize=fs_lab)
+ax[0].set_xlabel('Time (yr)', fontsize=fs_lab)
 ax[0].set_ylabel('Maximum vertical displacement (m)', fontsize=fs_lab)
 ax[0].grid(True, linestyle='dotted')
 ax[0].tick_params(axis='both', which='major', labelsize=fs)
@@ -86,12 +93,20 @@ ax[1].plot(comp_burgers_viscratio0pt5[:,0]/1e3, comp_burgers_viscratio0pt5[:,1],
 ax[1].plot(comp_burgers_viscratio0pt1[:,0]/1e3, comp_burgers_viscratio0pt1[:,1], color=colour_burgers_10, linewidth=lw, linestyle='-', label=r'Burgers (comp., $\eta_1 / \eta_2 = 10$)')
 
 # long, power law
-ax[1].plot(power_pt2_dt50[:,0]/1e3, power_pt2_dt50[:,1], linewidth=lw, linestyle='--', label=r'power dt 50 yr')
-ax[1].plot(power_pt2_dt125_nz20[:,0]/1e3, power_pt2_dt125_nz20[:,1], linewidth=lw, linestyle='--', label=r'power dt 125 yr, nz =20')
-ax[1].plot(power_pt2_dt125[:,0]/1e3, power_pt2_dt125[:,1], linewidth=lw, linestyle='--', label=r'power dt 125 yr')
-ax[1].plot(power_pt2_dt250[:,0]/1e3, power_pt2_dt250[:,1], linewidth=lw, linestyle='--', label=r'power dt 250 yr')
-ax[1].plot(power_pt2_dt500[:,0]/1e3, power_pt2_dt500[:,1], linewidth=lw, linestyle='--', label=r'power dt 500 yr')
+#ax[1].plot(power_pt2_dt50[:,0]/1e3, power_pt2_dt50[:,1], linewidth=lw, linestyle='--', label=r'power dt 50 yr')
+#ax[1].plot(power_pt2_dt125_nz20[:,0]/1e3, power_pt2_dt125_nz20[:,1], linewidth=lw, linestyle='--', label=r'power dt 125 yr, nz =20')
+#ax[1].plot(power_pt2_dt125[:,0]/1e3, power_pt2_dt125[:,1], linewidth=lw, linestyle='--', label=r'power dt 125 yr')
+#ax[1].plot(power_pt2_dt250[:,0]/1e3, power_pt2_dt250[:,1], linewidth=lw, linestyle='--', label=r'power dt 250 yr')
+#ax[1].plot(power_pt2_dt500[:,0]/1e3, power_pt2_dt500[:,1], linewidth=lw, linestyle='--', label=r'power dt 500 yr')
+#ax[1].plot(power_pt2_dt1000[:,0]/1e3, power_pt2_dt1000[:,1], linewidth=lw, linestyle='--', label=r'power dt 1000 yr')
+#ax[1].plot(power_pt2_dt2000[:,0]/1e3, power_pt2_dt2000[:,1], linewidth=lw, linestyle='--', label=r'power dt 2000 yr')
 
+#long power law coupled
+#ax[1].plot(power_pt2_dt125_coupled[:,0]/1e3, power_pt2_dt125_coupled[:,1], linewidth=lw, linestyle=':', label=r'power dt 125 yr, coupled')
+ax[1].plot(power_pt2_dt250_coupled[:,0]/1e3, power_pt2_dt250_coupled[:,1], linewidth=lw, linestyle=':', label=r'power dt 250 yr, coupled')
+#ax[1].plot(power_pt2_dt500_coupled[:,0]/1e3, power_pt2_dt500_coupled[:,1], linewidth=lw, linestyle=':', label=r'power dt 500 yr, coupled')
+#ax[1].plot(power_pt2_dt1000_coupled[:,0]/1e3, power_pt2_dt1000_coupled[:,1], linewidth=lw, linestyle=':', label=r'power dt 1000 yr, coupled')
+#ax[1].plot(power_pt2_dt2000_coupled[:,0]/1e3, power_pt2_dt2000_coupled[:,1], linewidth=lw, linestyle=':', label=r'power dt 2000 yr, coupled')
 
 ax[1].set_xlabel('Time (kyr)', fontsize=fs_lab)
 #ax[1].set_ylabel('Maximum vertical displacement (m)', fontsize=fs_lab)
